90cf1649b2f276f0653d233ea500ce7ac007a52c
Mariia Rozbitska <melissa180lily@gmail.com>
2023-05-17 14:04:30 +0300

    feat: add changes to js code

--- src/index.js
+++ src/index.js
@@ -1,44 +1,75 @@
 document.addEventListener("DOMContentLoaded", () => {
 
+	const modal = document.querySelector("#modal");
+	const btnOpenModal = document.querySelector("#modalOpen");
+	const btnConfirm = document.querySelector("#btnConfirm");
+	const btnCancel = document.querySelector("#btnCancel");
+	const close = document.getElementsByClassName("close")[0];
+	const modalContent = document.querySelector(".modal-body");
 
-	const openModalBtn = document.querySelectorAll('[data-modal-target]');
-	const closeModalBtn = document.querySelectorAll('[data-close-button]');
-	const overlay = document.querySelector('#overlay');
+	btnOpenModal.addEventListener ('click', function() {
+		modal.style.display = "block";
+		document.body.style.overflow = "hidden";
+	});
 
-	openModalBtn.forEach(button => {
-		button.addEventListener('click', () => {
-			const modal = document.querySelector(button.dataset.modalTarget);
-			openModal(modal)
-		})
-	})
 
-	overlay.addEventListener('click', () => {
-		const modals = document.querySelectorAll('.modal.active')
-		modals.forEach(modal => {
-			closeModal(modal)
-		})
-	})
+	modalContent.addEventListener('click', function(event) {
+		let target = event.target;
 
-	closeModalBtn.forEach(button => {
-		button.addEventListener('click', () => {
-			const modal = button.closest('.modal');
-			closeModal(modal => {
-				closeModal(modal)
-			})
-		})
-	})
+		if (target == close || btnCancel) {
+			modal.style.display = "none";
+			document.body.style.overflow = "visible";
+		} else if (target == btnConfirm) {
+			alert('OMG, this is so hard to explain');
+		} else {
+			return;
+		}
+	});
 
-	function openModal(modal) {
-		if (modal == null) return
-		modal.classList.add('.active')
-		overlay.classList.add('.active')
-	}
+	window.addEventListener = function(event) {
+	if (event.target == modal) {
+		modal.style.display = "none";
+	}};
+})
 
-	function closeModal(modal) {
-		if (modal == null) return
-		modal.classList.remove('.active')
-		overlay.classList.remove('.active')
-	}
+	//const openModalBtn = document.querySelectorAll('[data-modal-target]');
+	//const closeModalBtn = document.querySelectorAll('[data-close-button]');
+	//const overlay = document.querySelector('#overlay');
+
+	//openModalBtn.forEach(button => {
+		//button.addEventListener('click', () => {
+			//const modal = document.querySelector(button.dataset.modalTarget);
+			//openModal(modal)
+		//})
+	//})
+
+	//overlay.addEventListener('click', () => {
+		//const modals = document.querySelectorAll('.modal.active')
+		//modals.forEach(modal => {
+			//closeModal(modal)
+		//})
+	//})
+
+	//closeModalBtn.forEach(button => {
+		//button.addEventListener('click', () => {
+			//const modal = button.closest('.modal');
+			//closeModal(modal => {
+				//closeModal(modal)
+			//})
+		//})
+	//})
+
+	//function openModal(modal) {
+		//if (modal == null) return
+		//modal.classList.add('.active')
+		//overlay.classList.add('.active')
+	//}
+
+	//function closeModal(modal) {
+		//if (modal == null) return
+		//modal.classList.remove('.active')
+		//overlay.classList.remove('.active')
+	//}
 
 	//const body = document.querySelector('body');
 	//const modalBtn = document.querySelectorAll('.modal-btn__link');
@@ -58,6 +89,3 @@ document.addEventListener("DOMContentLoaded", () => {
 			//modalOverlay.classList.add('.modal-overlay--visible');
 		//});
 	//});
-
-
-})
