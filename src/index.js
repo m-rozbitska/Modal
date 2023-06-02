a10b432874e6cb01bacfd7947f66e3bfe40a4290
Mariia Rozbitska <melissa180lily@gmail.com>
2023-06-02 22:01:16 +0300

    feat: formatted code

--- src/index.js
+++ src/index.js
@@ -1,69 +1,16 @@
-
 document.addEventListener("DOMContentLoaded", () => {
-
-	(function () {
-		//'use strict'
-		const forms = document.querySelectorAll('.needs-validation')
-		Array.from(forms)
-		  .forEach(function (form) {
-			 form.addEventListener('submit', function (event) {
-			 if (!form.checkValidity()) {
-				  event.preventDefault()
-				  event.stopPropagation()
-			 }
-		  
-				form.classList.add('was-validated')
-			 }, false)
-		  })
-		})()
-
+  const forms = document.querySelectorAll(".needs-validation");
+  Array.from(forms).forEach(function (form) {
+    form.addEventListener(
+      "submit",
+      function (event) {
+        if (!form.checkValidity()) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+        form.classList.add("was-validated");
+      },
+      false
+    );
+  });
 });
-
-	// $(function () {
-	// 	$("#btnSubmit").on("click", function (e) {
-	// 		let form = $("#formValidate")[0];
-	// 		let isValid = form.checkValidity();
-	// 		if (!isValid) {
-	// 			e.preventDefault();
-	// 			e.stopPropagation();
-	// 		}
-	// 		form.classList.add('was-validated');
-	// 		return false;
-	// 	});
-	// });
-	
-
-	// (function() {
-	// 	window.addEventListener('load', function() {
-		
-	// 	  let forms = document.getElementsByClassName('needs-validation');
-	// 	  let validation = Array.prototype.filter.call(forms, function(form) {
-	// 		 form.addEventListener('submit', function(event) {
-	// 			if (form.checkValidity() === false) {
-	// 			  event.preventDefault();
-	// 			  event.stopPropagation();
-	// 			}
-	// 			form.classList.add('was-validated');
-	// 		 }, false);
-	// 	  });
-	// 	}, false);
-	//  })();
-	
-   
-
-// (function() {
-// 	window.addEventListener('load', function() {
-	
-// 	  let forms = document.getElementsByClassName('needs-validation');
-// 	  let validation = Array.prototype.filter.call(forms, function(form) {
-// 		 form.addEventListener('submit', function(event) {
-// 			if (form.checkValidity() === false) {
-// 			  event.preventDefault();
-// 			  event.stopPropagation();
-// 			}
-// 			form.classList.add('was-validated');
-// 		 }, false);
-// 	  });
-// 	}, false);
-//  })();
-
