40825b2def2f6c32db96114dc1cee5effd15813d
Mariia Rozbitska <melissa180lily@gmail.com>
2023-06-05 23:46:38 +0300

    feat: done first task

--- dist/index.975ef6c8.js
+++ dist/index.975ef6c8.js
@@ -557,33 +557,43 @@ function hmrAccept(bundle, id) {
 }
 
 },{}],"8lqZg":[function(require,module,exports) {
-fetch("https://jsonplaceholder.typicode.com/users", {
-    method: "POST",
-    body: JSON.stringify({
-        id: 3,
-        name: "Undefined",
-        username: "name777",
-        company: [
-            {
-                name: "Deleted"
-            }
-        ]
-    }),
-    headers: {
-        "Content-type": "application/json; charset=UTF-8"
-    }
-}).then((response)=>response.json()).then((json)=>console.log(json));
-const btn = document.querySelector(".request__btn");
-btn.addEventListener("click", function() {
-    fetch("https://jsonplaceholder.typicode.com/todos/1").then(function(data) {
-        console.log(data);
-        return data.json();
-    }).then(function(data) {
-        const todoResults = document.getElementById("todo__results");
-        todoResults.innerHTML = `<p>Todo User Id: ${data.id}</p>`;
-        console.log(data.id);
-    });
-});
+fetch("https://jsonplaceholder.typicode.com/users").then((response)=>response.json()).then((json)=>console.log(json)); // const users = 'https://jsonplaceholder.typicode.com/users';
+ // const btn = document.querySelector('.request__btn');
+ // btn.addEventListener('click', function() {
+ //   $.get(users, function(data, status){
+ //     console.log(`${data}`)
+ //   })
+ // })
+ // fetch('https://jsonplaceholder.typicode.com/users', {
+ //   method: 'POST',
+ //   body: JSON.stringify({
+ //     id: 3,
+ //     name: "Undefined",
+ //     username: "name777",
+ //     company: [ {
+ //         name: "Deleted",
+ //       }
+ //     ]  
+ //   }),
+ //   headers: {
+ //     'Content-type': 'application/json; charset=UTF-8',
+ //   },
+ // })
+ //   .then((response) => response.json())
+ //   .then((json) => console.log(json));
+ //   const btn = document.querySelector('.request__btn');
+ //   btn.addEventListener('click', function() {
+ //     fetch('https://jsonplaceholder.typicode.com/todos/1')
+ //     .then(function(data){
+ //       console.log(data)
+ //       return data.json()
+ //     })
+ //     .then(function(data){
+ //       const todoResults = document.getElementById('todo__results')
+ //       todoResults.innerHTML = `<p>Todo User Id: ${data.id}</p>`
+ //       console.log(data.id)
+ //     })
+ //   })
 
 },{}]},["cCBoz","8lqZg"], "8lqZg", "parcelRequire94c2")
 
--- src/index.js
+++ src/index.js
@@ -1,35 +1,50 @@
-fetch('https://jsonplaceholder.typicode.com/users', {
-  method: 'POST',
-  body: JSON.stringify({
-    id: 3,
-    name: "Undefined",
-    username: "name777",
-    company: [ {
-        name: "Deleted",
-      }
-    ]  
-  }),
-
-  headers: {
-    'Content-type': 'application/json; charset=UTF-8',
-  },
-})
+fetch('https://jsonplaceholder.typicode.com/users')
   .then((response) => response.json())
   .then((json) => console.log(json));
 
-  const btn = document.querySelector('.request__btn');
 
-  btn.addEventListener('click', function() {
+
+// const users = 'https://jsonplaceholder.typicode.com/users';
+// const btn = document.querySelector('.request__btn');
+
+// btn.addEventListener('click', function() {
+//   $.get(users, function(data, status){
+//     console.log(`${data}`)
+//   })
+// })
+
+// fetch('https://jsonplaceholder.typicode.com/users', {
+//   method: 'POST',
+//   body: JSON.stringify({
+//     id: 3,
+//     name: "Undefined",
+//     username: "name777",
+//     company: [ {
+//         name: "Deleted",
+//       }
+//     ]  
+//   }),
+
+//   headers: {
+//     'Content-type': 'application/json; charset=UTF-8',
+//   },
+// })
+//   .then((response) => response.json())
+//   .then((json) => console.log(json));
+
+//   const btn = document.querySelector('.request__btn');
+
+//   btn.addEventListener('click', function() {
     
-    fetch('https://jsonplaceholder.typicode.com/todos/1')
-    .then(function(data){
-      console.log(data)
-      return data.json()
-    })
-    .then(function(data){
-      const todoResults = document.getElementById('todo__results')
-      todoResults.innerHTML = `<p>Todo User Id: ${data.id}</p>`
-      console.log(data.id)
-    })
-  })
+//     fetch('https://jsonplaceholder.typicode.com/todos/1')
+//     .then(function(data){
+//       console.log(data)
+//       return data.json()
+//     })
+//     .then(function(data){
+//       const todoResults = document.getElementById('todo__results')
+//       todoResults.innerHTML = `<p>Todo User Id: ${data.id}</p>`
+//       console.log(data.id)
+//     })
+//   })
 
