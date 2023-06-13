71c16fbb12e397682274f5eaa1b820c24b84fcbb
Mariia Rozbitska <melissa180lily@gmail.com>
2023-06-13 13:10:29 +0300

    feat: change js code

--- src/index.js
+++ src/index.js
@@ -1,84 +1,40 @@
 document.addEventListener("DOMContentLoaded", () => {
 
-   let usersResult = document.querySelector('#usersResult');
-   let userBtn = document.querySelectorAll('.btn');
-  
-   
-
-   
    fetch('https://jsonplaceholder.typicode.com/users')
       .then((response) => response.json())
       .then((isUser) => {
-         if(isUser != null) {
-            isUser.forEach(element => {
-               usersResult.innerHTML +=
-               `<div class = "user__html-title" id="userTitleId${element.id}">
-               <p>Id:${element.id}</p><br>
-               <p>Name:${element.name}</p><br>
-               <button class="btn" id ="userBtn">User info: ${element.name}</button>
-               </div>
-            `;
-            });
-         };
-      });
-
-    
-
+            const usersResult = document.querySelector('#usersResult');
+            isUser.forEach(user => {
+               usersResult.insertAdjacentHTML('beforeend',
+               `<div>ID: ${user.id}</div>  
+               <div>NAME: ${user.name}</div>
+               <div><button data-user-id="${user.id}">FETCH TODOS</button></div>
+               <div class="todos" data-user-id="${user.id}" style="margin-bottom: 15px;"></div>
+            `);
+         });     
       
 
-      userBtn.forEach(elem => {
-         elem.addEventListener('click', function(event) {
-            event.preventDefault();
-            let userTodo =  elem.closest('.user__html-title').querySelector('#user__id').textContent;
+   const userBtns = document.querySelectorAll('button[data-user-id]');
 
-            fetch('https://jsonplaceholder.typicode.com/users/1/todos')
-               .then((response) => response.json())
-               .then((isUser) => {
-                  if (isUser[userTodo].id != null) {
-                        userBtn.closest('.user__html-title').querySelector('.user__info').innerHTML =
-                           `<p>User id: ${isUser[userTodo].userId}</p>
-                           ` } else {
-                              return;
-                  }
+      userBtns.forEach(button => {
+         button.addEventListener('click', function(event) {
+            event.preventDefault();
+            const userId = event.target.dataset.userId;
+            const todosWrapper = document.querySelector(`.todos[data-user-id="${userId}"]`);
+
+            fetch(`https://jsonplaceholder.typicode.com/users/${userId}/todos`)
+            .then(response => response.json())
+            .then(todos => {
+                     if(todos != null) {
+                     todos.forEach(todo => {
+                        todosWrapper.insertAdjacentHTML('beforeend', `
+                        <div>ID: ${todo.id}; TITLE: ${todo.title}; COMPLETED: ${todo.completed}</div>`);
+                     })
+                        } else {
+                        return;
+                     }
+                  })
                })
             })
-         })
-      });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-      
-
-//       userBtn.forEach(el => {
-//          el.addEventListener('click', function(event) {
-//             event.preventDefault();
-
-//             let userTodo =  el.closest('.user__html-title').querySelector('#user__id').textContent;
-
-//             fetch('https://jsonplaceholder.typicode.com/users/1/todos')
-//                      .then((response) => response.json())
-//                      .then((userHtml) => {
-//                         if (userHtml[userTodo].id != null) {
-//                            el.closest('.user__html-title').querySelector('.user__info').innerHTML =
-//                               `<p>User id: ${userHtml[userTodo].userId}</p>
-//                            `
-//                         } else {
-//                            return;
-//                         }
-//          })
-//       })
-// });
-
-
+         });
+       });
