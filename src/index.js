fb758bedc7152798024b29ae7d9f3396f03f9617
Mariia Rozbitska <melissa180lily@gmail.com>
2023-07-04 20:54:40 +0300

    init: start task

--- src/index.js
+++ src/index.js
@@ -335,7 +335,7 @@ document.addEventListener("DOMContentLoaded", () => {
                                        });
                                     } else {
                                        return;
-                                    }
+                        }
                      });
                   });
                });
