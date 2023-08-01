dc3bbf3075f69479997e42cab4cd6b5781a7f999
Mariia Rozbitska <melissa180lily@gmail.com>
2023-08-01 22:03:34 +0300

    init: start task

--- dist/index.975ef6c8.js
+++ dist/index.975ef6c8.js
@@ -557,73 +557,12 @@ function hmrAccept(bundle, id) {
 }
 
 },{}],"8lqZg":[function(require,module,exports) {
-document.addEventListener("DOMContentLoaded", ()=>{
-    const form = document.querySelector("#calc_form");
-    form.addEventListener("submit", (e)=>{
-        e.preventDefault();
-    });
-    const screen = document.querySelector("#screen");
-    const operand_btns = document.querySelectorAll("button[data-type=operand]");
-    const operator_btns = document.querySelectorAll("button[data-type=operator]");
-    const setDisplay = (inputS)=>{
-        if (inputS.length === 7) screen.style.fontSize = "3.7rem";
-        else if (inputS.length === 8) screen.style.fontSize = "3.3rem";
-        else if (inputS.length === 9) screen.style.fontSize = "3rem";
-        else if (inputS.length >= 10) display.style.fontSize = "2.6rem";
-    };
-    let equation = [];
-    operator_btns.forEach((btn)=>{
-        btn.addEventListener("click", (e)=>{
-            e.currentTarget.classList.add("active");
-            switch(e.target.value){
-                case "%":
-                    screen.value = Number(screen.value) / 100;
-                    break;
-                case "invert":
-                    screen.value = Number(screen.value) * -1;
-                    break;
-                case "=":
-                    equation.push(screen.value);
-                    screen.value = eval(equation.join(""));
-                    screen.value = Number(screen.value).toFixed(1);
-                    equation = [];
-                    break;
-                default:
-                    let last_item = equation[equation.length - 1];
-                    if ([
-                        "/",
-                        "*",
-                        "+",
-                        "-"
-                    ].includes(last_item) && is_operator) {
-                        equation.pop();
-                        equation.push(e.target.value);
-                    } else {
-                        equation.push(screen.value);
-                        equation.push(e.target.value);
-                    }
-                    is_operator = true;
-                    break;
-            }
-        });
-    });
-    let is_operator = false;
-    operand_btns.forEach((btn)=>{
-        btn.addEventListener("click", (e)=>{
-            if (screen.value == "0") screen.value = e.target.value;
-            else if (is_operator) {
-                is_operator = false;
-                screen.value = e.target.value;
-            } else if (screen.value.includes(".")) screen.value = screen.value + "" + e.target.value.replace(".", "");
-            else screen.value = screen.value + "" + e.target.value;
-        });
-    });
-    const remove_active = ()=>{
-        operator_btns.forEach((btn)=>{
-            btn.classList.remove("active");
-        });
-    };
-});
+const stringChecker = function(s, p1, p2) {
+    const result = (first, second)=>first + second === s.length || p1[first] === s[first + second] && result(first + 1, second) || p2[second] === s[first + second] && result(first, second + 1);
+    return s.length === p1.length + p2.length && result(0, 0);
+};
+console.log(stringChecker("radency", "rdnc", "aey"));
+console.log(stringChecker("radency", "rdnd", "aey"));
 
 },{}]},["cCBoz","8lqZg"], "8lqZg", "parcelRequire94c2")
 
